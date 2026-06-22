c520ec7a3156ead729309c3cd19a214de9b3faa9
Q-sedifo <oleggoper@ukr.net>
2026-06-22 23:53:04 +0200

    base settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", roboto.variable, oxaniumHeading.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
